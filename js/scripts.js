2b859df4a19a55e419680bd9ceafcf5e1d8aa720
David Torres <dftp93@gmail.com>
2020-06-01 17:34:41 -0500

    Added ./web/views/admin-create-sms.html to the project

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-// alert('Estable');
+
+'use strict'
+
+/* Preview SMS or USSD*/
+// The "is-invalid" style is added to all the inputs, if we press in the preview Checkbox, only for the purposes of the model.
+$('input#off_on_preview:checkbox').change(function () {
+  const newLocal = $('#form_sms .form_shields_validate');
+  if ($(this).is(":checked")) {
+    $(newLocal).removeClass("is-invalid");
+    console.log('Estable');
+  } else {
+    $(newLocal).addClass("is-invalid");
+    console.log('Estable');
+  }
+});
