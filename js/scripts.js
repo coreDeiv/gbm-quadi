0cd5df141cf88e4c687a130534f5d170df1edec6
David Torres <dftp93@gmail.com>
2020-06-01 18:11:42 -0500

    Added ./web/views/admin-create-ussd.html to the project

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,20 @@
 
 /* Preview SMS or USSD*/
 // The "is-invalid" style is added to all the inputs, if we press in the preview Checkbox, only for the purposes of the model.
+// sms inputs
 $('input#off_on_preview:checkbox').change(function () {
   const newLocal = $('#form_sms .form_shields_validate');
+  if ($(this).is(":checked")) {
+    $(newLocal).removeClass("is-invalid");
+    console.log('Estable');
+  } else {
+    $(newLocal).addClass("is-invalid");
+    console.log('Estable');
+  }
+});
+
+$('input#off_on_preview:checkbox').change(function () {
+  const newLocal = $('#form_ussd .form_shields_validate');
   if ($(this).is(":checked")) {
     $(newLocal).removeClass("is-invalid");
     console.log('Estable');
