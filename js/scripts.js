e58c714eb8e72845ef4f5a590aa74d0d5683f978
David Torres <dftp93@gmail.com>
2020-06-02 23:26:32 -0500

    Added sms-example-error, alerts to error or success and custom contextmenu on table rows

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 
 'use strict'
 
-
-
 jQuery(document).ready(function($) {
 
   /* Preview SMS or USSD*/
@@ -35,10 +33,35 @@ jQuery(document).ready(function($) {
   // All elements within a table in HTML, such as table, thead, tbody, th, tr, td, are invalid elements and you cannot add a link -
   // to them. Accordingly, the HTML5 API is used with the attribute data-<my-data-name>="".
 
-  // Click to href table row location
+  /* Click to href table row location */
+
   $(".clickable-row").click(function() {
       window.location = $(this).data("href");
       console.log("Estable");
   });
 
 });
+
+// No tener en cuenta esta funcion, para agregar el menu, ya que funciona con el efecto Hover
+const calcPosCm = () => {
+
+  // Callback icon and menu
+  const iconTableRow = document.querySelector(".fa-ellipsis-v");
+  const cmTr = document.querySelector(".custom-cm");
+
+  // Coordantes calculate
+  const iconTableRowX = iconTableRow.offsetRight;
+  const iconTableRowY = iconTableRow.offsetTop;
+
+  // Sizin ccalculate of contextmenu
+  const widthCm = cmTr.clientWidth;
+
+
+  const leftCm = iconTableRowX - (widthCm * 2) + 100;
+
+  cmTr.style.left = `${leftCm}px`;
+
+};
+
+window.addEventListener('load', () => calcPosCm());
+window.addEventListener('resize', () => calcPosCm());
